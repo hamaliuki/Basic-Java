3d1952d2bf59147bb5f061af593e5f61aca3dff3
hamaliuki <149725405+hamaliuki@users.noreply.github.com>
2023-11-21 22:51:20 +0100

    Add files via upload
    08.11.23

---
+++ hw08.11.23.1/char/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+        float myFloat = 4.7333436f;
+        double myDouble = 4.355453532;
+        long myLong = 12121;
+
+        System.out.println("char: " + myChar);
+        System.out.println("int: " + myInt);
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("long: " + myLong);
+    }
+}
